87048b2469f45b88a5bcf6917d0f9d675bed0057
Alex Meza <alex.meza.5471@gmail.com>
2025-12-09 20:51:03 -0600

    ejercicio 3 y ejercicio4

---
+++ certificacion_pcep/ejercicios/ejercicio3.py
@@ -0,0 +1,7 @@
+a : int = 10
+b : int = 5
+print(f"Suma : {a + b}")
+print(f"Resta : {a - b}")
+print(f"Multiplicación : {a * b}")
+print(f"División : {a / b}")
+print(f"Módulo : {a % b}")
---
+++ certificacion_pcep/ejercicios/ejercicio4.py
@@ -0,0 +1,8 @@
+numero_x : int  = 5
+numero_y : int =7
+numero_z : int = 3
+
+print(f"Suma de los tres números :  {numero_x + numero_y + numero_z}")
+print(f"Division entera de x/y y luego la resta con z : {(numero_x//numero_y)-numero_z}")
+print(f"La elevacion y**x y luego el modulo entre z : {(numero_y ** numero_x) % numero_z}")
+print((numero_y ** numero_x))
