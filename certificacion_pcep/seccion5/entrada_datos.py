8d04d45a584d7dbc7dc968699b7c34b40ad95a61
Alex Meza <alex.meza.5471@gmail.com>
2026-01-05 01:45:13 -0600

    Creacion de seccion 5. Enreada de datos y mostrar datos

---
+++ certificacion_pcep/seccion5/entrada_datos.py
@@ -0,0 +1,3 @@
+nombre : str = input("Ingrese su nombre completo : ")
+edad : int = int(input("Ingrese el valor de su edad : "))
+print(f"El  nombre ingresado es : {nombre} y la edad es {edad}")
---
+++ certificacion_pcep/seccion5/mostrar_datos.py
@@ -0,0 +1,3 @@
+nombre : str = input("Ingrese un nombre : ")
+edad : int = int(input("Ingresa tu edad : "))
+print(f"Mi nombre es : {nombre} y tengo {edad} años de edad.")
