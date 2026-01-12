3a15c6f9b2aae0ff020a08579acfbe2b2846559f
Alex Meza <alex.meza.5471@gmail.com>
2026-01-12 15:44:09 -0600

    Introduccion teorica a las listas con python

---
+++ certificacion_pcep/seccion6_estructuras/listas.py
@@ -0,0 +1,20 @@
+'''
+Teoria sobre las listas : 
+La listra es un tipo de coleccion ordenada. En otros lenguajes es un array.
+Espacios de memoria ordenados.
+
+Una tupla es una coleccion, pero no se pueden modificar. Es inmutable.
+
+Diccionarios : relacion  clave  - valor
+'''
+
+listaTest = ['Pan',40,37.5]
+tuplaTest = ('Alejandro','Aaron')
+dicTest = {'color1':'rosa','color2':'azul'}
+
+lista_frutas : str = ['manzana','platano','pera','guayaba']
+print(lista_frutas)
+print(lista_frutas[-1])
+print(lista_frutas[0:2])
+lista_frutas[0] = 'limon'
+print(lista_frutas)
