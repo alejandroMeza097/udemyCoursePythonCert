a8eb18bd77a5f0a0d91be5d83fd9f9737beee008
Alex Meza <alex.meza.5471@gmail.com>
2026-04-26 11:57:12 -0600

    Teoria sobre bucles anidados y sentencia break y continue

---
+++ certificacion_pcep/seccion7_flujo/break_continue.py
@@ -0,0 +1,10 @@
+
+i : int = 0
+while i <= 10:
+    if i == 6:
+        i = i + 1
+        continue
+    
+    print(f"Contador : {i}")
+    i = i + 1
+    
---
+++ certificacion_pcep/seccion7_flujo/bucles_anidados.py
@@ -0,0 +1,8 @@
+i : int = 1
+while i <= 3:
+    print(f"Grupo {i}")
+    j : int = 1
+    while j <= 5:
+        print(f"    Elemento {j}")
+        j = j + 1
+    i = i + 1
