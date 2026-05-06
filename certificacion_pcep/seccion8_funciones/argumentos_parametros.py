a8f5d583132ba9db3c6d775c328b858cf1dceae9
Alex Meza <alex.meza.5471@gmail.com>
2026-05-06 12:24:49 -0600

    Teoria sobre argumentos y parametros en funciones

---
+++ certificacion_pcep/seccion8_funciones/argumentos_parametros.py
@@ -0,0 +1,4 @@
+def saludo(nombre :str):
+    return f"Hola mi estimado(a) : {nombre}"
+
+print(saludo("Karime"))
---
+++ certificacion_pcep/seccion8_funciones/posiciones_parametros.py
@@ -0,0 +1,5 @@
+def saludar(nombre : str, mensaje : str) -> str:
+    return f"El nombre es : {nombre}. El mensaje es : {mensaje}"
+
+print(saludar("Gowyin","Esta dura la chamba"))
+print(saludar(mensaje="la vida es loca",nombre="vector"))
